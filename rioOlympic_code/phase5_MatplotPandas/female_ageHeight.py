9907070d907fe404818d75a9eac0d63e297cff15
Ruchi Sahu <sahruchi@gmail.com>
2017-05-20 23:37:16 -0700

    Update female_ageHeight.py

--- rioOlympic_code/phase5_MatplotPandas/female_ageHeight.py
+++ rioOlympic_code/phase5_MatplotPandas/female_ageHeight.py
@@ -1,9 +1,10 @@
-
 import matplotlib
 
 import pandas as pd
+import datetime as DT
 
 matplotlib.use('Agg')
+
 
 import matplotlib.pyplot as plt
 
@@ -16,31 +17,27 @@ import numpy as np
 
 data=pd.read_csv('https://raw.githubusercontent.com/ruchisahu/dataAnalysis_cloud9/master/olympic-games/athletes.csv')
 
+#convert dob to Age.
+now = pd.Timestamp(DT.datetime.now())
 
-#convert dob to Age.you can use the method, used previously
+data['dob'] = pd.to_datetime(data['dob'])  
 
-for i,row in enumerate(data.iterrows()):
+data['dob'] = data['dob'].where(data['dob'] < now, data['dob'] -  np.timedelta64(100, 'Y'))  
 
-    try:
-
-        data.loc[i,'Age'] = 116 - float(row[1].dob[len(row[1].dob)-2:len(row[1].dob)])
-
-    except TypeError:
-
-        data.loc[i,'Age'] = 0
+data['age'] = (now - data['dob']).astype('<m8[Y]')  
 
 
 #fillna function can “fill in” NA values with non-null data.Converted NaN values to ‘0’
 
-data['weight'] = data.weight.fillna(0)
 
 data['height'] = data.height.fillna(0)
 
-data = data[data.weight>0]
+data['age'] = data.age.fillna(0)
+
 
 data = data[data.height>0]
 
-data = data[data.Age>0]
+data = data[data.age>0]
 
 
 female = data[data.sex == 'female']
@@ -55,7 +52,7 @@ sports = ['hockey','boxing','cycling']
 
 for sport in sports:
 
-    plt.scatter(female.height[female.sport == sport],female.Age[female.sport == sport])
+    plt.scatter(female.height[female.sport == sport],female.age[female.sport == sport])
 
     legend.append(sport)
 
@@ -67,9 +64,9 @@ plt.legend(legend, loc = 2,numpoints=3)
 
 plt.title('female height and age  relation in sports?')
 
-plt.xlabel('Height [m]')
+plt.xlabel('Height')
 
-plt.ylabel('age [kg]')
+plt.ylabel('age')
 
 
 plt.show()
